76d264eb58c5e5bc9c3f13c8156119a08edae366
Matt Edwards <Matt.edwards94@gmail.com>
2019-03-01 16:28:52 +0000

    init commit

---
+++ loadImageFilename.py
@@ -0,0 +1,20 @@
+def loadImageFilename(flowType)
+    """ Switches based upon the input flowType
+        
+        Options:
+            1 - Backwards Facing Step
+            2 - Experimental Vortex
+            3 - Corner flow
+            4 - Jet Flow from PIV Challenge
+            5 - Buildings flow
+            6 - High Pressure Jet
+            7 - Wall Jet
+            8 - Lungs Bifurcation
+            9 - 
+    
+    """
+
+
+
+if __name__ == "__main__":
+    print("hello")
