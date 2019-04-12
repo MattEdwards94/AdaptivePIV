5240e80b3cd9ea4bec34f150b16d03a422388cff
Matt Edwards <m.edwards@bristol.ac.uk>
2019-04-12 15:22:20 +0100

    Initial attempt at extracting values (i.e. windows) from the image
    extracts the intensity values and mask values from the piv_image object centred at x_ctr, y_ctr with half width rad

--- piv_image.py
+++ piv_image.py
@@ -111,9 +111,10 @@ class piv_image:
         """
 
         # initialises the output
-        ia = np.zeros((2 * rad + 1, 2 * rad + 1))
-        ib = np.zeros((2 * rad + 1, 2 * rad + 1))
-        mask = np.zeros((2 * rad + 1, 2 * rad + 1))
+        WS = 2 * rad + 1
+        ia = np.zeros((WS, WS))
+        ib = np.zeros((WS, WS))
+        mask = np.zeros((WS, WS))
 
         # determine the region of the window which lies within the image
         l = max(min(x_ctr - rad - 1, self.img_details.n_cols), 0)
@@ -132,7 +133,8 @@ class piv_image:
         # now read values
         ia[bStart:tEnd][:, lStart:rEnd] = self.IA[b:t][:, l:r]
         ib[bStart:tEnd][:, lStart:rEnd] = self.IB[b:t][:, l:r]
-        mask[bStart:tEnd][:, lStart:rEnd] = self.mask[b:t][:, l:r]
+        if self.img_details.has_mask:
+            mask[bStart:tEnd][:, lStart:rEnd] = self.mask[b:t][:, l:r]
 
         return ia, ib, mask
 
@@ -144,14 +146,24 @@ if __name__ == "__main__":
     img = piv_image(img_details, 1)
     print(img_details)
 
-    start = time.time()
-    # for i in range(0, 10):
     ia, ib, mask = img.get_region(24, 24, 10)
     print(ia)
     print(ib)
     print(mask)
+
+    start = time.time()
+    for i in range(0, 10000):
+        ia, ib, mask = img.get_region(24, 24, 10)
     end = time.time()
     print(end - start)
+
+    print(repr(img))
+    img_2 = eval(repr(img))
+    if img_2 == img:
+        print("Yay, it worked")
+
+    en = time.time()
+    print("Time: {}".format(en - start))
 
     # print(ia)
     """for ii in range(1, 5):
