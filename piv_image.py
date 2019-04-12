89c54e8bf49ec8edab8e4cce3f4fcc7b8002693d
Matt Edwards <m.edwards@bristol.ac.uk>
2019-04-12 10:21:27 +0100

    Adds function to get sub-region from large image
    Not yet tested to make sure that it is extracting the right region, I already suspect an off by 1 error

--- piv_image.py
+++ piv_image.py
@@ -5,6 +5,7 @@ import h5py
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 class piv_image:
@@ -66,22 +67,67 @@ class piv_image:
         return "piv_image(image_info.ImageObject({}), {})".format(
             self.img_details.flowtype, self.img_number)
 
+    def get_region(self, x_ctr, y_ctr, rad):
+        """retrieves the pixel intensities for the region requested.
+        If the region requested extends beyond the image dimensions, then
+        such pixels will be set to 0 intensity.
+
+        Args:
+            x_ctr (TYPE): Description
+            y_ctr (TYPE): Description
+            rad (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
+
+        # initialises the output
+        ia = np.zeros((2 * rad + 1, 2 * rad + 1))
+        ib = np.zeros((2 * rad + 1, 2 * rad + 1))
+        mask = np.zeros((2 * rad + 1, 2 * rad + 1))
+
+        # determine the region of the window which lies within the image
+        l = max(min(x_ctr - rad - 1, self.img_details.n_cols), 0)
+        r = max(min(x_ctr + rad - 1, self.img_details.n_cols), 0)
+        b = max(min(y_ctr - rad - 1, self.img_details.n_rows), 0)
+        t = max(min(y_ctr + rad - 1, self.img_details.n_rows), 0)
+
+        # now determine where in the output window the data should sit
+        # i.e. if the window overlaps over the right hand edge of the image
+        # then there are going to be 0's on the rhs of the corr window
+        lStart = max(l - (x_ctr - rad), 1)
+        rEnd = lStart + (r - l)
+        bStart = max(b - (y_ctr - rad), 1)
+        tEnd = bStart + t - b
+
+        # now read values
+        ia[bStart:tEnd][:, lStart:rEnd] = self.IA[b:t][:, l:r]
+        ib[bStart:tEnd][:, lStart:rEnd] = self.IB[b:t][:, l:r]
+        mask[bStart:tEnd][:, lStart:rEnd] = self.mask[b:t][:, l:r]
+
+        return ia, ib, mask
+
 
 if __name__ == "__main__":
     img_info.list_available_flowtypes()
     print('loading image details for BFS')
     img_details = img_info.ImageInfo(1)
-    print(img_details)
-    # img = piv_image(img_details, 1)
-
-    img_details = img_info.ImageInfo(19)
+    img = piv_image(img_details, 1)
     print(img_details)
 
-    for ii in range(1, 27):
+    start = time.time()
+    # for i in range(0, 10):
+    ia, ib, mask = img.get_region(24, 24, 10)
+    print(ia)
+    print(ib)
+    print(mask)
+    end = time.time()
+    print(end - start)
+
+    # print(ia)
+    """for ii in range(1, 5):
         img_details = img_info.ImageInfo(ii)
         print(ii)
         img = piv_image(img_details, 1)
         print(img.IA[0][0:10])
-        img = piv_image(img_details, img_details.n_images)
-        print(img.IA[0][0:10])
-    plt.show()
+    plt.show()"""
