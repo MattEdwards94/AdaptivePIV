d2bbd821c3a0ee428bba2f252f9c8d31546cdb93
Matt <matt.edwards94@gmail.com>
2019-05-16 12:14:52 +0100

    Move unit test code into tests subfolder

--- testingCAPI.py
+++
@@ -1,6 +0,0 @@
-# Though it looks like an ordinary python import, the addList module
-# is implemented in C
-import addList
-
-li = [1, 2, 3, 4, 5]
-print("Sum of List - {} = {}".format(li, addList.add(li)))
