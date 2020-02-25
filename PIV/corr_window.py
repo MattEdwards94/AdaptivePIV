2e9777ec095023fee94fb6b5720adbb1577ca9a2
MattEdwards94 <Matt.edwards94@gmail.com>
2020-02-25 18:09:12 +0000

    Bugfix: Handle if the corrmap is all zeros
    If the corrmap (or infact, just the 3x3 region around the peak)
    is all zeros then we will get a divide by zero error.
    
    If this is the case then just return zero displacement with blank SNR

--- PIV/corr_window.py
+++ PIV/corr_window.py
@@ -253,9 +253,13 @@ class CorrWindow:
         corrmap = calculate_correlation_map(wsa, wsb, self.WS, self.rad)
 
         # find the subpixel displacement from the correlation map
-        self.u, self.v, self.SNR = cyth_corr_window.get_disp_from_corrmap(
-            corrmap, self.WS, self.rad)
-        # print(f"u: {self.u}, v: {self.v}, SNR: {self.SNR}")
+        try:
+            self.u, self.v, self.SNR = cyth_corr_window.get_disp_from_corrmap(
+                corrmap, self.WS, self.rad)
+        except ZeroDivisionError:
+            # happens if all values around the peak are the same value.
+            self.u, self.v, self.SNR = 0, 0, 1
+            return self.u, self.v, self.SNR
 
         # combine displacement with predictor
         u_avg, v_avg = dp.get_local_avg_disp(self.x, self.y, self.rad)
