5d7ff8bffa7ca390cef9121b9826e392a2a154bf
MattEdwards94 <m.edwards@bristol.ac.uk>
2020-03-22 11:46:22 +0000

    Generalise ensemble widim (#80)
    * Format: Change docstring format
    
    * Update: Make ensemble analysis general

--- PIV/analysis.py
+++ PIV/analysis.py
@@ -13,18 +13,33 @@ from PIV.utilities import vprint
 ESSENTIAL, BASIC, TERSE = 1, 2, 3
 
 
-def ensemble_widim(flowtype, im_start, im_stop, settings):
+def ensemble_analysis(method, flowtype, im_start, im_stop, settings):
     """Analyses an ensemble of images and returns an EnsembleSolution object
 
-    Args:
-        flowtype (int): The flow type id. See image_info.all_flow_types()
-        im_start (int): The number of the first image in the series to analyse
-        im_stop (int): The number of the last image in the series to analyse
-                       Inclusive. i.e. 1-3 will analyse 1, 2, and 3
-        settings (dict): Settings to analyse the images with
+    Parameters
+    ----------
+    method : function handle
+        Handle to the function which is supposed to be used for the ensemble
+        analysis. 
+        Examples: 'widim', 'structured_adaptive_analysis' or 'adaptive_analysis'
+    flowtype : int
+        The flow type id. See image_info.all_flow_types()
+    im_start : int
+        The number of the first image in the series to analyse
+    im_stop : int
+        The number of the last image in the series to analyse 
+        Inclusive. i.e. 1-3 will analyse 1, 2, and 3
+    settings : SettingsClass
+        Settings to analyse the images with. Must agree with the method
+        e.g. 
+        'widim' uses WidimSettings
+        'structured_adaptive_analysis' uses AdaptStructSettings
+        'adaptive_analysis' uses AdaptSettings
 
-    Returns:
-        EnsembleSolution: An EnsembleSolution object. See ensemble_solution.py
+    Returns
+    solution: EnsembleSolution
+        An EnsembleSolution object. 
+        See ensemble_solution.py
     """
 
     # set the verbosity level
@@ -35,8 +50,8 @@ def ensemble_widim(flowtype, im_start, im_stop, settings):
 
     for i in range(im_start, im_stop + 1):
         vprint(1, "Analysing image {}".format(i))
-        dp = widim(PIV.PIVImage.from_flowtype(flowtype, i),
-                   settings)
+        dp = method(PIV.PIVImage.from_flowtype(flowtype, i),
+                    settings)
         ensR.add_displacement_field(dp)
 
     # reset verbosity
